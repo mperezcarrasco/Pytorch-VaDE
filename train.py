500726ae9fa7a4059fb31f3fdce344a293335150
mperezcarrasco <ma.ignacioperezc@gmail.com>
2020-02-10 12:26:11 -0300

    solving gamma errors

--- train.py
+++ train.py
@@ -150,8 +150,8 @@ class TrainerVaDE:
     def compute_gamma(self, z, p_c):
         h = (z.unsqueeze(1) - self.VaDE.mu_prior).pow(2) / self.VaDE.log_var_prior.exp(); print(self.VaDE.mu_prior.shape, h.shape)
         h += self.VaDE.log_var_prior; print(h.shape)
-        h += np.log(np.pi*2); print(h.shape)
-        p_z_c = torch.exp(torch.log(p_c) - 0.5 * torch.sum(h)); print( p_z_c.shape, torch.log(p_c).shape)
+        h += np.log(np.pi*2); print(h.shape, torch.sum(h).shape)
+        p_z_c = torch.exp(torch.log(p_c).unsqueeze(0) - 0.5 * torch.sum(h)); print( p_z_c.shape, torch.log(p_c).shape)
         gamma = p_z_c / torch.sum(p_z_c, dim=1, keepdim=True)
         return gamma
 

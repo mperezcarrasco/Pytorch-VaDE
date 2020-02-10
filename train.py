07fcec56893a368c6f9dd99ff5e6af539fb04433
mperezcarrasco <ma.ignacioperezc@gmail.com>
2020-02-09 23:37:06 -0300

    changes in loss

--- train.py
+++ train.py
@@ -130,13 +130,15 @@ class TrainerVaDE:
     def compute_loss(self, x, x_hat, mu, log_var, z):
         gamma = self.compute_gamma(z)
 
-        log_p_x_given_z = F.binary_cross_entropy(x_hat, x, reduction='mean')
+        log_p_x_given_z = F.binary_cross_entropy(x_hat, x, reduction='sum')
         h = log_var.exp().unsqueeze(1) + (mu.unsqueeze(1) - self.VaDE.mu_prior).pow(2)
         h = torch.sum(self.VaDE.log_var_prior + h / self.VaDE.log_var_prior.exp(), dim=2)
         log_p_z_given_c = 0.5 * torch.sum(gamma * h)
         log_p_c = torch.sum(gamma * torch.log(self.VaDE.pi_prior + 1e-9))
         log_q_c_given_x = torch.sum(gamma * torch.log(gamma + 1e-9))
         log_q_z_given_x = 0.5 * torch.sum(1 + log_var)
+
+        print(log_p_x_given_z, log_p_z_given_c, log_p_c,   log_q_c_given_x, log_q_z_given_x)
 
         loss = log_p_x_given_z + log_p_z_given_c + log_p_c -  log_q_c_given_x - log_q_z_given_x
         loss /= x.size(0)

c58fe7570a5d1ba914d830b77912347193fb5f4c
mperezcarrasco <ma.ignacioperezc@gmail.com>
2020-02-10 15:45:53 -0300

    VaDE

--- preprocess.py
+++ preprocess.py
@@ -4,16 +4,16 @@ from torch.utils.data import DataLoader, TensorDataset
 
 
 def get_mnist(data_dir='./data/mnist/',batch_size=128):
-    train=MNIST(root=data_dir,train=True,download=True)
-    test=MNIST(root=data_dir,train=False,download=True)
+    train = MNIST(root=data_dir, train=True, download=True)
+    test = MNIST(root=data_dir, train=False, download=True)
 
-    X=torch.cat([train.data.float().view(-1,784)/255., test.data.float().view(-1,784)/255.], 0)
-    Y=torch.cat([train.targets, test.targets], 0)
+    x = torch.cat([train.data.float().view(-1,784)/255., test.data.float().view(-1,784)/255.], 0)
+    y = torch.cat([train.targets, test.targets], 0)
 
-    dataset=dict()
-    dataset['X']=X
-    dataset['Y']=Y
+    dataset = dict()
+    dataset['x'] = x
+    dataset['y'] = y
 
-    dataloader=DataLoader(TensorDataset(X,Y), batch_size=batch_size, 
+    dataloader=DataLoader(TensorDataset(x,y), batch_size=batch_size, 
                           shuffle=True, num_workers=0)
     return dataloader

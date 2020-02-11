c4b2f5fb3b67796f0742b91f8b85112a46155abb
mperezcarrasco <ma.ignacioperezc@gmail.com>
2020-02-10 21:32:47 -0300

    VaDE

--- train.py
+++ train.py
@@ -133,25 +133,25 @@ class TrainerVaDE:
         gamma = self.compute_gamma(z, p_c)
 
         log_p_x_given_z = F.binary_cross_entropy(x_hat, x, reduction='mean')
-        h = (mu.unsqueeze(1) - self.VaDE.mu_prior.unsqueeze(0)).pow(2)
+        h = (mu.unsqueeze(1) - self.VaDE.mu_prior).pow(2)
         h += log_var.exp().unsqueeze(1)
-        h = torch.sum(self.VaDE.log_var_prior.unsqueeze(0) +\
-            h / self.VaDE.log_var_prior.exp().unsqueeze(0), dim=2)
-        log_p_z_given_c = 0.5 * torch.mean(torch.sum(gamma * h, dim=1))
-        log_p_c = torch.mean(torch.sum(gamma * torch.log(p_c.unsqueeze(0) + 1e-9), dim=1))
-        log_q_c_given_x = torch.mean(torch.sum(gamma * torch.log(gamma + 1e-9), dim=1))
-        log_q_z_given_x = 0.5 * torch.mean(torch.sum(1 + log_var, dim=1))
-        print(log_p_x_given_z, log_p_z_given_c, log_p_c, log_q_c_given_x, log_q_z_given_x)
+        h = torch.sum(self.VaDE.log_var_prior + h / self.VaDE.log_var_prior.exp(), dim=2)
+        log_p_z_given_c = 0.5 * torch.sum(gamma * h, dim=1)
+        log_p_c = torch.sum(gamma * torch.log(p_c + 1e-9))
+        log_q_c_given_x = torch.sum(gamma * torch.log(gamma + 1e-9))
+        log_q_z_given_x = 0.5 * torch.sum(1 + log_var)
+
         loss = log_p_x_given_z + log_p_z_given_c - log_p_c + log_q_c_given_x - log_q_z_given_x
+        loss /= x.size(0)
 
         return loss
     
     def compute_gamma(self, z, p_c):
-        h = (z.unsqueeze(1) - self.VaDE.mu_prior.unsqueeze(0)).pow(2)
-        h /= self.VaDE.log_var_prior.exp().unsqueeze(0)
-        h += self.VaDE.log_var_prior.unsqueeze(0)
+        h = (z.unsqueeze(1) - self.VaDE.mu_prior).pow(2)
+        h /= self.VaDE.log_var_prior.exp()
+        h += self.VaDE.log_var_prior
         h += torch.Tensor([np.log(np.pi*2)]).to(self.device)
-        p_z_c = torch.exp(torch.log(p_c + 1e-9).unsqueeze(0) - 0.5 * torch.sum(h, dim=2))
+        p_z_c = torch.exp(torch.log(p_c + 1e-9) - 0.5 * torch.sum(h, dim=2))
         gamma = p_z_c / torch.sum(p_z_c + 1e-9, dim=1, keepdim=True)
         return gamma
 

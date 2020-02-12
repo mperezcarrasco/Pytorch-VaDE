781bdf68a0ecd4e0d300ce13cbbc6084b5fdb928
mperezcarrasco <ma.ignacioperezc@gmail.com>
2020-02-12 10:09:37 -0300

    mnist working 90acc

--- train.py
+++ train.py
@@ -57,7 +57,7 @@ class TrainerVaDE:
                     x = self.feature_extractor(x)
                     x = x.detach()
                 x_hat = self.autoencoder(x)
-                loss = F.binary_cross_entropy(x_hat, x, reduction='mean') #reconstruction error
+                loss = F.mse_loss(x_hat, x, reduction='mean') #reconstruction error
                 loss.backward()
                 optimizer.step()
                 total_loss += loss.item()
@@ -155,9 +155,9 @@ class TrainerVaDE:
 
     def compute_loss(self, x, x_hat, mu, log_var, z):
         p_c = self.VaDE.pi_prior
-        gamma = self.compute_gamma(z, p_c)
+        gamma = self.compute_gamma(z, p_c); print(gamma)
 
-        log_p_x_given_z = F.binary_cross_entropy(x_hat, x, reduction='sum')
+        log_p_x_given_z = F.mse_loss(x_hat, x, reduction='sum')
         h = log_var.exp().unsqueeze(1) + (mu.unsqueeze(1) - self.VaDE.mu_prior).pow(2)
         h = torch.sum(self.VaDE.log_var_prior + h / self.VaDE.log_var_prior.exp(), dim=2)
         log_p_z_given_c = 0.5 * torch.sum(gamma * h)
@@ -173,7 +173,7 @@ class TrainerVaDE:
         h = (z.unsqueeze(1) - self.VaDE.mu_prior).pow(2) / self.VaDE.log_var_prior.exp()
         h += self.VaDE.log_var_prior
         h += torch.Tensor([np.log(np.pi*2)]).to(self.device)
-        p_z_c = torch.exp(torch.log(p_c + 1e-9).unsqueeze(0) - 0.5 * torch.sum(h, dim=2))
+        p_z_c = torch.exp(torch.log(p_c + 1e-9).unsqueeze(0) - 0.5 * torch.sum(h, dim=2)) + 1e-9
         gamma = p_z_c / torch.sum(p_z_c, dim=1, keepdim=True)
         return gamma
 
